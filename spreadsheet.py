11c0e4dc62bf39ad72f995db7e60be534ccb6513
Beyond <beyondboyplays@gmail.com>
2021-05-29 11:43:03 +0530

    Changed sps and util

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,14 +1,10 @@
 # Import other libraries to be used in your code
 import coc
-import creds
 import util
 import io 
 
-# Here you make the connection to the COC API using the coc.py library
-coc_client = coc.login(creds.coc_dev_email, creds.coc_dev_password, client=coc.EventsClient, key_names="war_home")
-
-coc_dev_email = "beyondboyplays@gmail.com"  # Clash of Clans Developer Account email
-coc_dev_password = "Munna345"  # Clash of Clans Developer Account password
+coc_dev_email = "EMAIL"  # Clash of Clans Developer Account email
+coc_dev_password = "Password"  # Clash of Clans Developer Account password
 fname="fullroster.csv"
 
 async def main():
@@ -17,7 +13,8 @@ async def main():
 
 	with io.open(fname, "w", encoding="utf-8") as f:
 		f.write(roster_data)
-
+	
 client = coc.login(coc_dev_email ,coc_dev_password)
 client.loop.run_until_complete(main())
 #client.close()
+
